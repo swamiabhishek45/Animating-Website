86bd86157e1c4bdb8331c11259c7896a9ee3b59a
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-19 19:42:14 +0530

    Update HTML and CSS for soccer club website

---
+++ script.js
@@ -0,0 +1,37 @@
+var cursor = document.querySelector(".cursor");
+var cursorBlur = document.querySelector(".cursor-blur");
+
+document.addEventListener("mousemove", function(dets){
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
+    cursorBlur.style.left = dets.x - 150 + "px";
+    cursorBlur.style.top = dets.y - 150 + "px";
+})
+
+
+
+gsap.to(".nav", {
+  backgroundColor: "#000",
+  height: "110px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".nav",
+    scroller: "body",
+    // markers: true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+
+gsap.to(".main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: ".main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 1,
+  },
+});
